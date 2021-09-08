011d196f8424f011d9799225941e15720ac0b310
Maksym Zyza <mmziza88@gmail.com>
2021-09-08 12:20:18 +0300

    Revert "Revert "Revert "changed routes for deploy app"""
    This reverts commit 46a69fc2cc76044cae74d8b031238ecd3ad04470.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
-app.use("/hotDog", hotDogRouter);
+app.use("/", hotDogRouter);
 
 // app.get("/", (req, res) => {
 //   res.send("NODEMON WORK!");
