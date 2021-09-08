22ddc0b4ebb9a75e70855a6351f88cf9bf0a6963
Maksym Zyza <mmziza88@gmail.com>
2021-09-08 12:10:15 +0300

    changed routes for deploy app

--- routes/hotdog.routes.js
+++ routes/hotdog.routes.js
@@ -2,14 +2,10 @@ const Router = require("express");
 const router = new Router();
 const hotDogController = require("../controller/hotdog.controller");
 
-router.get("/", function (req, res) {
-  res.json({ message: "hotDog servise - hosting + /hotDog" });
-});
-
-router.post("/hotDog", hotDogController.createHotDog);
-router.get("/hotDog", hotDogController.getHotDogs);
-router.get("/hotDog/:id", hotDogController.getOneHotDog);
-router.put("/hotDog", hotDogController.updateHotDog);
-router.delete("/hotDog/:id", hotDogController.deleteHotDog);
+router.post("/", hotDogController.createHotDog);
+router.get("/", hotDogController.getHotDogs);
+router.get("/:id", hotDogController.getOneHotDog);
+router.put("/", hotDogController.updateHotDog);
+router.delete("/:id", hotDogController.deleteHotDog);
 
 module.exports = router;
