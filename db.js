e6e46502ec8f3a2e6f1582164493e918c78aafd4
Maksym Zyza <mmziza88@gmail.com>
2021-09-09 13:02:17 +0300

    Created PostgreSQL database for deploy in Heroku

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
+
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+
 const pool = new Pool({
-  user: "postgres",
-  password: "root",
-  host: "localhost",
-  port: 5434,
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
   // ssl: {
   //   rejectUnauthorized: false,
